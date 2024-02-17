9c402b2ac0168d550ab9dc5c02ed056e60f11226
konul <Konul.Eminova@windmill.digital>
2024-02-17 17:28:19 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# simple weather app - flutter
+
+Display weather details on your current or any specific location.
+
+## Assignment parameters
+
+Perform two tasks on the repo:
+
+1. Create or extend a feature. For example: add a weather forecast, let the user switch between Fahrenheit, Kelvin and Celsius, etc.
+2. Refactor existing code (feature or architectural)
+
+During the interview we will go over your tasks. You will be asked to explain your code and reasoning and to give us a little (tech) roadmap of what else you would like to see done in the code.
+We expect you to spend no more than 3 hours on the assignment. 
+
+Please send the resulting code via Git or zip to q@tapelectric.app and nico@tapelectric.app 3 hours in advance of our interview.
+
+## Screenshot
+
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/169255578-c60cc40b-aafa-4dc0-9db2-30f9850efb74.jpg" >
+</p>
---
+++ lib/utils/location.dart
@@ -0,0 +1,35 @@
+import 'package:geolocator/geolocator.dart';
+
+class UserLocation {
+  double? latitude;
+  double? longitude;
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    print('Permission ${permission}');
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    latitude = position.latitude;
+    longitude = position.longitude;
+    return position;
+  }
+}
